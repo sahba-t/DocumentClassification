e23f41b91d6d32787d218812cb452d3c71e7aa19
Mauricio Monsivais <mmonsivais@unm.edu>
2020-03-18 02:08:13 -0600

    --trying to create sparse matrix out of testing data

--- src/make_sparse_lr.py
+++ src/make_sparse_lr.py
@@ -3,20 +3,15 @@ import numpy as np
 from scipy import sparse
 from scipy.sparse import csr_matrix, lil_matrix
 
-ResourcesFolder = "../res/"
-sparse_training = lil_matrix((12000, 61189), dtype=np.int16)
-labels = np.zeros(12000, dtype=np.int8)
-sparse_training[:, -1] = np.ones((12000, 1), dtype=np.int16)
-i = 0
-with open(ResourcesFolder + 'training.csv', 'r') as train_stream:
-    for line in train_stream:
+sparse_training = lil_matrix((6774, 61189), dtype=np.int16)
+sparse_training[:, -1] = np.ones((6774, 1), dtype=np.int16)
+with open('../res/testing.csv', 'r') as train_stream:
+    for i, line in enumerate(train_stream):
+        print(i, len(line))
         current_line = np.array(list(map(int, line.split(','))), dtype=np.int16)
-        labels[i] = current_line[-1]
-        sparse_training[i,:-1] = current_line[1:-1] / np.sum(current_line[1:-1])
-        i += 1
-        print(i)
-sparse.save_npz(ResourcesFolder + 'lr_training_data_norm', sparse_training.tocsr())
+        sparse_training[i, :-1] = current_line[1:]
+sparse.save_npz('../res/lr_testing_data', sparse_training.tocsr())
 print(sparse_training)
 print(sparse_training.shape)
-print(sparse_training[2,-1])
-print(sparse_training[2,-2])
+print(sparse_training[2, -1])
+print(sparse_training[2, -2])
